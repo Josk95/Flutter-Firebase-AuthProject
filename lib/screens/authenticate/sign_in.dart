a5d0489d7b00f71b4d7289abf285cd477c8c5030
Josk95 <jonas.skoog95@gmail.com>
2021-11-26 10:00:20 +0100

    SignIn
    created signin screen

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth_app/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final AuthService _authService = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[400],
+        elevation: 0.0,
+        title: const Text('Sign in'),
+      ),
+      body: Container(
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+          child: ElevatedButton(
+              onPressed: () async {
+                dynamic result = await _authService.signInAnon();
+                if (result == null) {
+                  print('error signing in');
+                } else {
+                  print('signed in');
+                  print(result);
+                }
+              },
+              child: const Text('Sign In Anonymously'))),
+    );
+  }
+}
