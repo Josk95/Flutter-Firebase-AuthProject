2d0212a4c2d2c2076db0264803f00fca9e72aa52
Josk95 <jonas.skoog95@gmail.com>
2021-11-26 09:35:17 +0100

    Created an anonymously signin function

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,1 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 
+class AuthService {
+  //Instance of Firebase Auth
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //Sign in Anonymously
+  Future signInAnon() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      return result.user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //Sign in Email
+
+  //Register with email
+
+  //Sign Out
+
+}
