03032f5071852e0583655fde11504448dbce8c42
Josk95 <jonas.skoog95@gmail.com>
2021-11-26 12:10:21 +0100

    Sign In Form
    Created a signIn form

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -11,28 +11,55 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _authService = AuthService();
 
+  //Text Field state
+  String email = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[100],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[400],
-        elevation: 0.0,
-        title: const Text('Sign in'),
-      ),
-      body: Container(
+        backgroundColor: Colors.blue[100],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[400],
+          elevation: 0.0,
+          title: const Text('Sign in'),
+        ),
+        body: Container(
           padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
-          child: ElevatedButton(
-              onPressed: () async {
-                dynamic result = await _authService.signInAnon();
-                if (result == null) {
-                  print('error signing in');
-                } else {
-                  print('signed in');
-                  print(result.uid);
-                }
-              },
-              child: const Text('Sign In Anonymously'))),
-    );
+          child: Form(
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                TextFormField(
+                  onChanged: (value) {
+                    setState(() {
+                      email = value;
+                    });
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  obscureText: true,
+                  onChanged: (value) {
+                    setState(() {
+                      password = value;
+                    });
+                  },
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.blue[700],
+                        textStyle: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold)),
+                    onPressed: () async {
+                      print(email);
+                      print(password);
+                    },
+                    child: Text('Sign in'))
+              ],
+            ),
+          ),
+        ));
   }
 }
