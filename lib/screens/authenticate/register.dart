061832cae67f280a10d36c79dfc4576027d2addc
Josk95 <jonas.skoog95@gmail.com>
2021-11-26 12:22:33 +0100

    Fixed UI for register screen

---
+++ lib/screens/authenticate/register.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_auth_app/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final AuthService _authService = AuthService();
+
+  //Text Field state
+  String email = "";
+  String password = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue[100],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[400],
+          elevation: 0.0,
+          title: const Text('Register'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+          child: Form(
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                TextFormField(
+                  onChanged: (value) {
+                    setState(() {
+                      email = value;
+                    });
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  obscureText: true,
+                  onChanged: (value) {
+                    setState(() {
+                      password = value;
+                    });
+                  },
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.blue[700],
+                        textStyle: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold)),
+                    onPressed: () async {
+                      print(email);
+                      print(password);
+                    },
+                    child: Text('Register'))
+              ],
+            ),
+          ),
+        ));
+  }
+}
