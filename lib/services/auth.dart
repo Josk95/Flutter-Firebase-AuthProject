3ec468ae94af5385766c189adbe6b148daba30d1
Josk95 <jonas.skoog95@gmail.com>
2021-11-26 12:27:30 +0100

    Created signIn With email function

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -28,6 +28,16 @@ class AuthService {
   }
 
   //Sign in Email
+  Future signInWithEmail(email, password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   //Register with email
 
